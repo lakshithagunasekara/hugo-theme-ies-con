2d5af61348853819ddfe461a8f4b5e099cb5ab9e
lakshithagunasekara <lakshithacrsg@gmail.com>
2024-04-03 23:31:30 +1100

    Remove anchor link button

--- static/js/theme.js
+++ static/js/theme.js
@@ -1519,7 +1519,6 @@ ready( function(){
     initOpenapi();
     initMenuScrollbar();
     initToc();
-    initAnchorClipboard();
     initCodeClipboard();
     fixCodeTabs();
     restoreTabSelections();
